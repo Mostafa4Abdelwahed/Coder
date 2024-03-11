3eb063b8581e9e63a60b7c85e4582382fc1c797f
Mostafa <mostafaabdalwahed4567@gmail.com>
2024-03-11 21:27:16 +0200

    Edit Link Request Api

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const request = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://coder-academy-api.onrender.com"
 })
 
 export default request
