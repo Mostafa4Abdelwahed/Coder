9802dbb22a7f4f0784aa8216c6738b7167561009
Mostafa Abdelwahed <65525706+Mostafa4Abdelwahed@users.noreply.github.com>
2025-07-09 03:57:20 +0300

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const request = axios.create({
-    baseURL: "https://coder-academy-api.onrender.com"
+    baseURL: "https://backend-coder-api.vercel.app"
 })
 
 export default request
